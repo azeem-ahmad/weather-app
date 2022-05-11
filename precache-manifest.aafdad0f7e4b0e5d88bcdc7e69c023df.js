6547f159b9034aa5fcb1ba5e833badcfd9b06a3d
Azeem Ahmad <41817419+azeem-ahmad@users.noreply.github.com>
2022-05-11 18:29:47 +0530

    upload all files

---
+++ precache-manifest.aafdad0f7e4b0e5d88bcdc7e69c023df.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d83304bf4f323d3839db7833269ad0a5",
+    "url": "/weather-app/index.html"
+  },
+  {
+    "revision": "05d20856cbeae969ca50",
+    "url": "/weather-app/static/css/main.ecda29ec.chunk.css"
+  },
+  {
+    "revision": "17bb53fcdd865cdd9a3b",
+    "url": "/weather-app/static/js/2.ad3b9c4a.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/weather-app/static/js/2.ad3b9c4a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "05d20856cbeae969ca50",
+    "url": "/weather-app/static/js/main.787b63b7.chunk.js"
+  },
+  {
+    "revision": "933af7f4ac4be227ef6e",
+    "url": "/weather-app/static/js/runtime-main.88d99ec0.js"
+  },
+  {
+    "revision": "d1cee715f935d9cf7bf49ba1c559e0b0",
+    "url": "/weather-app/static/media/cold-bg.d1cee715.jpg"
+  },
+  {
+    "revision": "744da03906ec4bfa41a31b2c6188fed7",
+    "url": "/weather-app/static/media/default-bg.744da039.jpg"
+  },
+  {
+    "revision": "12984940c51b844f4df234749fd9e5a6",
+    "url": "/weather-app/static/media/very-warm-bg.12984940.jpg"
+  },
+  {
+    "revision": "17c5248f0db0daea4a63acfd3ad1f3ac",
+    "url": "/weather-app/static/media/warm-bg.17c5248f.jpg"
+  }
+]);
